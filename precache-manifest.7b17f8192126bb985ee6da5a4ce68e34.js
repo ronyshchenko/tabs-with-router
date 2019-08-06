02c8a47717e38b1b52f290bd3ec67f859be6c6ff
ronyshchenko <roma.onyshchenko@gmail.com>
2019-08-06 08:36:02 +0300

    Updates

---
+++ precache-manifest.7b17f8192126bb985ee6da5a4ce68e34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e303875dba841b890d8166d43126fcf9",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "e1ab69f88c9a6740d289",
+    "url": "/react_tabs-with-router/static/css/main.ce953078.chunk.css"
+  },
+  {
+    "revision": "f30889fd01f16bee8e09",
+    "url": "/react_tabs-with-router/static/js/2.22515542.chunk.js"
+  },
+  {
+    "revision": "e1ab69f88c9a6740d289",
+    "url": "/react_tabs-with-router/static/js/main.9c47aece.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
